7713e8da4c561b8e74ab5f90290d2e65c3848a76
Toghrul <30516889+Btogrul@users.noreply.github.com>
2024-11-04 15:16:26 +0400

    Update CorsConfig.java

--- src/main/java/com/example/locationproject/config/CorsConfig.java
+++ src/main/java/com/example/locationproject/config/CorsConfig.java
@@ -30,7 +30,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 //.allowedOriginPatterns("*")
-                .allowedOrigins("https://qerbiazerbaycanim.com")
+                .allowedOrigins("https://qerbiazerbaycanim.com/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
