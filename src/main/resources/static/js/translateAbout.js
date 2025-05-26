7f424be280a48b4e2c8fbabd13342853e3b3e439
Toghrul <baghirli.togrul@gmail.com>
2025-05-26 17:09:55 +0400

    new translate update

--- src/main/resources/static/js/translateAbout.js
+++ src/main/resources/static/js/translateAbout.js
@@ -4,14 +4,15 @@ const translations = {
         text2: "contact us",
         ideaAuthorTitle: "Author of the Idea and Project",
         ideaAuthorDescription: "Asif Sevindik Muradzade was born in 1969. He is originally from the village of Agdu in the Garakilse district of the Zangezur region of Western Azerbaijan. He graduated from the Azerbaijan Technology Institute and the Azerbaijan State University of Economics. He has worked in various positions at the State Customs Committee of the Republic of Azerbaijan and the Ministry of Taxes of the Republic of Azerbaijan. He is currently an employee of the State Tax Service of the Republic of Azerbaijan's Main Department of Local Revenues in Baku.",
-
+        ideaAuthorDescription2: "He is the author of the novels \"Black Sail\" and \"Zangazur. Devil love\".",
         aboutAuthorH: "We express our gratitude to those who participated in the project."
     },
     az: {
         homePage: "Ana Səhifə",
         text2: "əlaqə",
         ideaAuthorTitle: "İdea və layihə müəllifi",
-        ideaAuthorDescription: "Asif Sevindik oğlu Muradzadə 1969-cu ildə anadan olub. Əslən Qərbi Azərbaycanın Zəngəzur mahalının Qarakilsə rayonunun Ağdü kəndindəndir. Azərbaycan Texnologiya İnstitutunu və Azərbaycan Dövlət İqtisad Universitetini bitirmişdir. Azərbaycan Respublikası Dövlət Gömrük Komitəsində və Azərbaycan Respublikası Vergilər Nazirliyində müxtəlif fəaliyyət göstərmişdir. Hazırda Azərbaycan Respublikasının İqtisadiyyat Nazirliyi yanında Dövlət Vergi Xidmətinin Bakı şəhəri Lokal Gəlirlər Baş İdarəsinin əməkdaşıdır.",
+        ideaAuthorDescription: "Asif Sevindik oğlu Muradzadə 1969-cu ildə anadan olub. Əslən Qərbi Azərbaycanın Zəngəzur mahalının Qarakilsə rayonunun Ağdü kəndindəndir. Azərbaycan Texnologiya İnstitutunu və Azərbaycan Dövlət İqtisad Universitetini bitirmişdir. Azərbaycan Respublikası Dövlət Gömrük Komitəsində və Azərbaycan Respublikası Vergilər Nazirliyində müxtəlif vəzifələrdə fəaliyyət göstərmişdir. Hazırda Azərbaycan Respublikasının İqtisadiyyat Nazirliyi yanında Dövlət Vergi Xidmətinin Bakı şəhəri Lokal Gəlirlər Baş İdarəsinin əməkdaşıdır.",
+        ideaAuthorDescription2: "\"Qara Yelkən\" və \"Zəngəzur. İblis eşqi\" romanlarının müəllifidir.",
         aboutAuthorH: "Layihədə iştirak edənlərə öz təşəkkürümüzü bildiririk."
 
     }
@@ -59,6 +60,7 @@ function applyTranslations(lang) {
     document.getElementById("head-contact-btn").innerText = translations[lang].text2;
     document.getElementById("idea-author-title").innerText = translations[lang].ideaAuthorTitle;
     document.getElementById("idea-author-description").innerText = translations[lang].ideaAuthorDescription;
+    document.getElementById("idea-author-description2").innerText = translations[lang].ideaAuthorDescription2;
     document.getElementById("thanks-guys").innerText = translations[lang].aboutAuthorH;
 }
 
